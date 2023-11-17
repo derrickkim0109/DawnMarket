8ecc26d7b1e42988c1c933b57f7f7441f2f2c8ae
derrickkim0109 <derrickkim0109@naver.com>
2023-11-17 23:14:53 +0900

    add: 기본 뷰 추가

---
+++ TimFresh_Assignment/Application/RootView.swift
@@ -0,0 +1,17 @@
+//
+//  RootView.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RootView: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
