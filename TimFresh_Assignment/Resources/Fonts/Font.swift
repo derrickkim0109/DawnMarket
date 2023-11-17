71012dcac8e85c38558166f623fb753a963c9a4b
derrickkim0109 <derrickkim0109@naver.com>
2023-11-17 23:13:58 +0900

    feat: Font, Image 파일이름에 따라 호출하는 코드 추가

---
+++ TimFresh_Assignment/Resources/Fonts/Font.swift
@@ -0,0 +1,69 @@
+//
+//  UIFont.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/17/23.
+//
+
+import SwiftUI
+
+public extension Font {
+    static func pretendard(size fontSize: CGFloat, type: PretendardFontType) -> Font {
+        return .custom("\(type.name)", size: fontSize)
+    }
+
+    static func spoqaHanSansNeo(size fontSize: CGFloat, type: SpoqaHanSansNeoFontType) -> Font {
+        return .custom("\(type.name)", size: fontSize)
+    }
+}
+
+public extension Font {
+    enum SpoqaHanSansNeoFontType {
+        case bold
+        case medium
+        case regular
+
+        var name: String {
+            switch self {
+            case .bold:
+                return "SpoqaHanSansNeo-Bold"
+            case .medium:
+                return "SpoqaHanSansNeo-Medium"
+            case .regular:
+                return "SpoqaHanSansNeo-Regular"
+            }
+        }
+    }
+
+    enum PretendardFontType {
+        case black
+        case bold
+        case extraBold
+        case extraLight
+        case light
+        case medium
+        case semiBold
+        case thin
+
+        var name: String {
+            switch self {
+            case .black:
+                return "Pretendard-Black"
+            case .bold:
+                return "Pretendard-Bold"
+            case .extraBold:
+                return "Pretendard-ExtraBold"
+            case .extraLight:
+                return "Pretendard-ExtraLight"
+            case .light:
+                return "Pretendard-Light"
+            case .medium:
+                return "Pretendard-Medium"
+            case .semiBold:
+                return "Pretendard-SemiBold"
+            case .thin:
+                return "Pretendard-Thin"
+            }
+        }
+    }
+}
---
+++ TimFresh_Assignment/Resources/Fonts/Image.swift
@@ -0,0 +1,24 @@
+//
+//  Images.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/17/23.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let launchScreenLogo = Image("launchScreenLogo")
+    static let launchScreenBackgroundImage = Image("launchScreenBackgroundImage")
+    static let iconBellImage = Image("iconBellImage")
+    static let iconSearchImage = Image("iconSearchImage")
+    static let iconSettingImage = Image("iconSettingImage")
+    static let iconPersonImage = Image("iconPersonImage")   
+    static let iconHamburgerImage = Image("iconHamburgerImage")
+    static let iconStarImage = Image("iconStarImage")
+    static let iconHomeImage = Image("iconHomeImage")
+    static let iconCartImage = Image("iconCartImage")
+    static let iconCartFillImage = Image("iconCartFillImage")
+    static let iconBackButtonImage = Image("iconBackButtonImage")
+    static let iconNewBadgeImage = Image("iconNewBadgeImage")
+}
