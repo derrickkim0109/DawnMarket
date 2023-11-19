3f5e6fd2f2655de52130578609fe3af3b3a974c7
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:08:06 +0900

    feat: AppMainQuickMenuFetchUseCase 구현
    - DomainLayer
    - AppMainQuickMenuFetchError

---
+++ TimFresh_Assignment/DomainLayer/UseCase/AppMainQuickMenu/AppMainQuickMenuFetchUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  AppMainQuickMenuFetchUseCase.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Combine
+import Foundation
+
+protocol AppMainQuickMenuFetchUseCaseInterface {
+    func fetchAppMainQuickMenuList() -> AnyPublisher<AppMainQuickMenuFetchEntity, AppMainQuickMenuFetchError>
+}
+
+final class AppMainQuickMenuFetchUseCase: AppMainQuickMenuFetchUseCaseInterface {
+    private let repository: AppMainQuickMenuFetchRepositoryInterface
+
+    init(repository: AppMainQuickMenuFetchRepositoryInterface) {
+        self.repository = repository
+    }
+
+    func fetchAppMainQuickMenuList() -> AnyPublisher<AppMainQuickMenuFetchEntity, AppMainQuickMenuFetchError> {
+        return repository.fetchAppMainQuickMenuList()
+            .mapAppMainQuickMenuFetchError()
+    }
+}
