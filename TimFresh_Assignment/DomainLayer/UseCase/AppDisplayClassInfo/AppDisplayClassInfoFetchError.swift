88fcec84f75c7b84f2652647a5c4e0f2d5090008
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:04:59 +0900

    feat: AppDisplayClassInfoFetchUseCase 구현
    - Domain
    - AppDisplayClassInfoFetchError

---
+++ TimFresh_Assignment/DomainLayer/UseCase/AppDisplayClassInfo/AppDisplayClassInfoFetchError.swift
@@ -0,0 +1,22 @@
+//
+//  AppDisplayClassInfoFetchError.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Foundation
+import Combine
+
+enum AppDisplayClassInfoFetchError: String, Error {
+    case failToFindAppDisplayClassInfoList = "카테고리 대분류를 불러오는데 실패했습니다."
+}
+
+extension Publisher where Output == AppDisplayClassInfoFetchEntity {
+    func mapAppDisplayClassInfoListFetchError() -> AnyPublisher<Output, AppDisplayClassInfoFetchError> {
+        return mapError { _ in
+            AppDisplayClassInfoFetchError.failToFindAppDisplayClassInfoList
+        }
+        .eraseToAnyPublisher()
+    }
+}
---
+++ TimFresh_Assignment/DomainLayer/UseCase/AppDisplayClassInfo/AppDisplayClassInfoFetchUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  AppDisplayClassInfoFetchUseCase.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Combine
+import Foundation
+
+protocol AppDisplayClassInfoFetchUseCaseInterface {
+    func fetchAppDisplayClassInfoList() -> AnyPublisher<AppDisplayClassInfoFetchEntity, AppDisplayClassInfoFetchError> 
+}
+
+final class AppDisplayClassInfoFetchUseCase: AppDisplayClassInfoFetchUseCaseInterface {
+    private let repository: AppDisplayClassInfoFetchRepositoryInterface
+
+    init(repository: AppDisplayClassInfoFetchRepositoryInterface) {
+        self.repository = repository
+    }
+
+    func fetchAppDisplayClassInfoList() -> AnyPublisher<AppDisplayClassInfoFetchEntity, AppDisplayClassInfoFetchError> {
+        return repository.fetchAppDisplayClassInfoList()
+            .mapAppDisplayClassInfoListFetchError()
+    }
+}
