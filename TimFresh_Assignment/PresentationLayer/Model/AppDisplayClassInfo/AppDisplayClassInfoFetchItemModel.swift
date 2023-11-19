b06e66655247e5ea3aa2dba377e88754ceaff477
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:05:38 +0900

    feat: AppDisplayClassInfoFetchModel 구현
    - Presentation Layer

---
+++ TimFresh_Assignment/PresentationLayer/Model/AppDisplayClassInfo/AppDisplayClassInfoFetchItemModel.swift
@@ -0,0 +1,27 @@
+//
+//  AppDisplayClassInfoFetchItemModel.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Foundation
+
+struct AppDisplayClassInfoFetchItemModel: Identifiable {
+    public let id = UUID()
+    let displayClassCode: String
+    let displayClassImagePath: String
+    let displayClassName: String
+    let displayClassSequence: Int64
+}
+
+extension AppDisplayClassInfoFetchItemModel {
+#if DEBUG
+    static let completedMock = AppDisplayClassInfoFetchItemModel(
+        displayClassCode: "DPDCH23097",
+        displayClassImagePath: "https://d1afu5va4iy6dc.cloudfront.net/disp-clas-info/51/8703f654-207e-425f-8543-8b63f56f5e41.jpg",
+        displayClassName: "농축수산물",
+        displayClassSequence: 51
+    )
+#endif
+}
---
+++ TimFresh_Assignment/PresentationLayer/Model/ModelMapper/AppDisplayClassInfoFetchModelMapper.swift
@@ -0,0 +1,27 @@
+//
+//  AppDisplayClassInfoFetchModelMapper.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+struct AppDisplayClassInfoFetchModelMapper {
+    static func toPresentationModel(entity: AppDisplayClassInfoFetchEntity) -> AppDisplayClassInfoFetchModel {
+
+        let appDisplayClassInfoModelList = entity.data.map {
+            let imagePath = $0.makeImagePath()
+
+            return AppDisplayClassInfoFetchItemModel(
+                displayClassCode: $0.displayClassCode,
+                displayClassImagePath: imagePath,
+                displayClassName: $0.displayClassName,
+                displayClassSequence: $0.displayClassSequence
+            )
+        }
+
+        return AppDisplayClassInfoFetchModel(
+            data: appDisplayClassInfoModelList,
+            message: entity.message
+        )
+    }
+}
