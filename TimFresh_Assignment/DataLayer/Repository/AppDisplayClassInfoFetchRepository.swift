063550a63e10be8b22590edcb1c6551fc2744f11
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:04:33 +0900

    feat: AppDisplayClassInfoFetchRepository
    - DataLayer
    - 구현체

---
+++ TimFresh_Assignment/DataLayer/Repository/AppDisplayClassInfoFetchRepository.swift
@@ -0,0 +1,26 @@
+//
+//  AppDisplayClassInfoFetchRepository.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Combine
+import Moya
+
+final class AppDisplayClassInfoFetchRepository : AppDisplayClassInfoFetchRepositoryInterface {
+    private let dataSource: AppDisplayClassInfoDataSourceInterface
+
+    init(dataSource: AppDisplayClassInfoDataSourceInterface) {
+        self.dataSource = dataSource
+    }
+
+    func fetchAppDisplayClassInfoList() -> AnyPublisher<AppDisplayClassInfoFetchEntity, MoyaError> {
+        return dataSource.fetchAppDisplayClassInfoList()
+            .map { responseDTO in
+                let entity = responseDTO.toEntity()
+                return entity
+            }
+            .eraseToAnyPublisher()
+    }
+}
