4e7b8e6e7e2ba4948a695ca144b63fdbdc74839c
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:06:22 +0900

    add: ListResultAppMainQuickMenuResponseDTO 추가

---
+++ TimFresh_Assignment/DataLayer/DTO/Response/AppMainQuickMenu/ListResultAppMainQuickMenuResponseDTO.swift
@@ -0,0 +1,31 @@
+//
+//  ListResultAppMainQuickMenuResponseDTO.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Foundation
+
+struct ListResultAppMainQuickMenuResponseDTO: Decodable {
+    let data: [AppMainQuickMenuResponseDTO]
+    let message: String?
+
+    func toEntity() -> AppMainQuickMenuFetchEntity {
+        let appMainQuickMenuFetchEntityList = data.map { $0.toEntity() }
+
+        return AppMainQuickMenuFetchEntity(
+            data: appMainQuickMenuFetchEntityList,
+            message: message ?? ""
+        )
+    }
+}
+
+extension ListResultAppMainQuickMenuResponseDTO {
+    #if DEBUG
+    static let completedMock = ListResultAppMainQuickMenuResponseDTO(
+        data: [AppMainQuickMenuResponseDTO.completedMock],
+        message: nil
+    )
+    #endif
+}
