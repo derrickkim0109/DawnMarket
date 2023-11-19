9f572058aae2507ce1416595cccf5ee2a36f2c12
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:09:54 +0900

    add: 카테고리 하위목록 조회 API DTO
    - Request
    - Response

---
+++ TimFresh_Assignment/DataLayer/DTO/Request/AppDisplayClassInfoBySubDisplayClassInfoRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  AppDisplayClassInfoBySubDisplayClassInfoRequestDTO.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Foundation
+
+struct AppDisplayClassInfoBySubDisplayClassInfoRequestDTO {
+    let displayClassSequence: Int
+}
---
+++ TimFresh_Assignment/DataLayer/DTO/Response/AppDisplayBySubClass/AppSubDisplayClassInfoResponseDTO.swift
@@ -0,0 +1,46 @@
+//
+//  AppSubDisplayClassInfoResponseDTO.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Foundation
+
+struct AppSubDisplayClassInfoResponseDTO: Decodable {
+    let displayClassCode: String?
+    let displayClassLevel: String?
+    let displayClassSequence: Int?
+    let parentsDisplayClassSequence: Int?
+    let subDisplayClassName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case displayClassCode = "dispClasCd"
+        case displayClassLevel = "dispClasLvl"
+        case displayClassSequence = "dispClasSeq"
+        case parentsDisplayClassSequence = "prntsDispClasSeq"
+        case subDisplayClassName = "subDispClasNm"
+    }
+
+    func toEntity() -> AppSubDisplayClassInfoFetchItemEntity {
+        return AppSubDisplayClassInfoFetchItemEntity(
+            displayClassCode: displayClassCode ?? "",
+            displayClassLevel: displayClassLevel ?? "",
+            displayClassSequence: displayClassSequence ?? 0,
+            parentsDisplayClassSequence: parentsDisplayClassSequence ?? 0,
+            subDisplayClassName: subDisplayClassName ?? ""
+        )
+    }
+}
+
+extension AppSubDisplayClassInfoResponseDTO {
+#if DEBUG
+    static let completedMock = AppSubDisplayClassInfoResponseDTO(
+        displayClassCode: "DPDCM23091",
+        displayClassLevel: "중분류",
+        displayClassSequence: 52,
+        parentsDisplayClassSequence: 51,
+        subDisplayClassName: "채소과일"
+    )
+#endif
+}
