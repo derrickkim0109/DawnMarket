6f2f5f2056822d43fa5ac12452b133f90592f265
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 00:11:33 +0900

    feat: 카테고리 하위목록 조회 UseCase 구현
    - Domain Layer

---
+++ TimFresh_Assignment/DomainLayer/UseCase/AppDisplayBySubClass/AppDisplayBySubClassFetchUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  AppDisplayBySubClassFetchUseCase.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/18/23.
+//
+
+import Combine
+import Foundation
+
+protocol AppDisplayBySubClassFetchUseCaseInterface {
+    func fetch(by displayClassSequence: Int) -> AnyPublisher<AppDisplayClassInfoBySubDisplayClassFetchEntity, AppDisplayBySubClassFetchError>
+}
+
+final class AppDisplayBySubClassFetchUseCase: AppDisplayBySubClassFetchUseCaseInterface {
+    private let repository: AppDisplayBySubClassFetchRepositoryInterface
+
+    init(repository: AppDisplayBySubClassFetchRepositoryInterface) {
+        self.repository = repository
+    }
+
+    func fetch(by displayClassSequence: Int) -> AnyPublisher<AppDisplayClassInfoBySubDisplayClassFetchEntity, AppDisplayBySubClassFetchError> {
+        let requestDTO = AppDisplayClassInfoBySubDisplayClassInfoRequestDTO(
+            displayClassSequence: displayClassSequence
+        )
+
+        return repository.fetch(requestDTO: requestDTO)
+            .mapAppDisplayBySubClassFetchError()
+    }
+}
