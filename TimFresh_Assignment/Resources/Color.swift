58624846cf71c090f1d1f3ecfa76664267c13fd8
derrickkim0109 <derrickkim0109@naver.com>
2023-11-20 22:40:21 +0900

    add: Color, Image 추가

---
+++ TimFresh_Assignment/Resources/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  TimFresh_Assignment
+//
+//  Created by Derrick kim on 11/20/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandDarkNavyColor = Color("brandDarkNavyColor")
+    static let appDisplayClassTextColor = Color("appDisplayClassTextColor")
+    static let dividerViewBackgroundColor = Color("dividerViewBackgroundColor")
+    static let appMainQuickMenuBackgroundColor = Color("appMainQuickMenuBackgroundColor")
+    static let appMainQuickMenuTextColor = Color("appMainQuickMenuTextColor")
+}
