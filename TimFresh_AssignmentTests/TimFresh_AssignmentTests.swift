791cd8b12b576ce943c04ae78a6a6a306e916a34
derrickkim0109 <derrickkim0109@naver.com>
2023-11-26 13:11:16 +0900

    add: UnitTest 생성

---
+++ TimFresh_AssignmentTests/TimFresh_AssignmentTests.swift
@@ -0,0 +1,14 @@
+//
+//  TimFresh_AssignmentTests.swift
+//  TimFresh_AssignmentTests
+//
+//  Created by Derrick kim on 11/26/23.
+//
+
+import XCTest
+
+final class TimFresh_AssignmentTests: XCTestCase {
+    func testExample() throws {
+
+    }
+}
