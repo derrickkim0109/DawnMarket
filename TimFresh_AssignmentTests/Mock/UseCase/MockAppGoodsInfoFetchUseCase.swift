737ce3d8bfedb908370c17e371f3a5d990426fd6
derrickkim0109 <derrickkim0109@naver.com>
2023-11-27 22:39:54 +0900

    add: MockAppGoodsInfoFetchUseCase 구현

---
+++ TimFresh_AssignmentTests/Mock/UseCase/MockAppGoodsInfoFetchUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  MockAppGoodsInfoFetchUseCase.swift
+//  TimFresh_AssignmentTests
+//
+//  Created by Derrick kim on 11/27/23.
+//
+
+@testable import TimFresh_Assignment
+import Combine
+
+final class MockAppGoodsInfoFetchUseCase: AppGoodsInfoFetchUseCaseInterface {
+    var scenario: Scenario = .success
+    var entity: AppGoodsInfoFetchEntity = AppGoodsInfoEntityObjectMother.getAppGoodsInfoFetchEntityWithCompleteData()
+
+    func fetch(request: AppGoodsInfoFetchRequestValue) -> AnyPublisher<AppGoodsInfoFetchEntity, AppGoodsInfoFetchError> {
+        switch scenario {
+        case .success:
+            return Just(entity)
+                .setFailureType(to: AppGoodsInfoFetchError.self)
+                .eraseToAnyPublisher()
+        case .failure:
+            return Fail(error: AppGoodsInfoFetchError.failToFindAppGoodsInfoList)
+                .eraseToAnyPublisher()
+        }
+    }
+}
