541916be963b83462e33537733014eacc8bfb595
derrickkim0109 <derrickkim0109@naver.com>
2023-11-27 22:39:16 +0900

    add: AppDisplayClassInfoFetchUseCase 구현

---
+++ TimFresh_AssignmentTests/Mock/UseCase/MockAppDisplayClassInfoFetchUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  MockAppDisplayClassInfoFetchUseCase.swift
+//  TimFresh_AssignmentTests
+//
+//  Created by Derrick kim on 11/27/23.
+//
+
+@testable import TimFresh_Assignment
+import Combine
+
+final class MockAppDisplayClassInfoFetchUseCase: AppDisplayClassInfoFetchUseCaseInterface {
+
+    var scenario: Scenario = .success
+    var entity: AppDisplayClassInfoFetchEntity = AppDisplayClassEntityObjectMother.getAppDisplayClassInfoFetchEntityWithCompleteData()
+
+    func fetchAppDisplayClassInfoList() -> AnyPublisher<AppDisplayClassInfoFetchEntity, AppDisplayClassInfoFetchError> {
+        switch scenario {
+        case .success:
+            return Just(entity)
+                .setFailureType(to: AppDisplayClassInfoFetchError.self)
+                .eraseToAnyPublisher()
+        case .failure:
+            return Fail(error: AppDisplayClassInfoFetchError.failToFindAppDisplayClassInfoList)
+                .eraseToAnyPublisher()
+        }
+    }
+}
