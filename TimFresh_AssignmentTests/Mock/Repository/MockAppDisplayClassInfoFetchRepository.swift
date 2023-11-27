27b8865328978c57655b64197e578e4451f7c84e
derrickkim0109 <derrickkim0109@naver.com>
2023-11-27 22:20:53 +0900

    add: MockAppDisplayClassInfoFetchRepository 구현

---
+++ TimFresh_AssignmentTests/Mock/Repository/MockAppDisplayClassInfoFetchRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MockAppDisplayClassInfoFetchRepository.swift
+//  TimFresh_AssignmentTests
+//
+//  Created by Derrick kim on 11/27/23.
+//
+
+import Foundation
+import Moya
+import Combine
+@testable import TimFresh_Assignment
+
+final class MockAppDisplayClassInfoFetchRepository: AppDisplayClassInfoFetchRepositoryInterface {
+    var scenario: Scenario = .success
+    var entity: AppDisplayClassInfoFetchEntity = AppDisplayClassEntityObjectMother.getAppDisplayClassInfoFetchEntityWithCompleteData()
+
+    func fetchAppDisplayClassInfoList() -> AnyPublisher<AppDisplayClassInfoFetchEntity, MoyaError> {
+        switch scenario {
+        case .success:
+            return Just(entity)
+                .setFailureType(to: MoyaError.self)
+                .eraseToAnyPublisher()
+        case .failure:
+            return Fail(error: MoyaError.statusCode(Response(statusCode: 400, data: Data())))
+                .eraseToAnyPublisher()
+        }
+    }
+}
