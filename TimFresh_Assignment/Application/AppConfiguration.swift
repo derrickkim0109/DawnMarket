29a4872114dff1ab5570368180613e8e320c67ea
derrickkim0109 <derrickkim0109@naver.com>
2023-11-29 23:54:21 +0900

    chore: 문자열 변경

--- TimFresh_Assignment/Application/AppConfiguration.swift
+++ TimFresh_Assignment/Application/AppConfiguration.swift
@@ -11,7 +11,7 @@ final class AppConfiguration {
     lazy var apiBaseURL: String = {
         guard let apiBaseURL = Bundle.main.object(
             forInfoDictionaryKey: "BASE_URL") as? String else {
-            fatalError("APIBaseURL must not be empty in plist")
+            fatalError("BASE_URL must not be empty in plist")
         }
         return apiBaseURL
     }()    
